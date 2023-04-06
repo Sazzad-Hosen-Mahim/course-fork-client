85b0e4bec01203e13e462c9f6f3cf6ae6d24ec6f
Sazzad Hosen <sajjadhosenmahim@gmail.com>
2023-04-07 05:20:12 +0600

    course category page fixed

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,9 +1,15 @@
 import React from "react";
+import CourseCategories from "../Courses/CourseCategories/CourseCategories";
 
 const Category = () => {
   return (
-    <div>
-      <h1>This is category page</h1>
+    <div className="grid grid-cols-12 mt-10 gap-3">
+      <div className="col-span-3 text-2xl border-2 ms-3 p-5">
+        <CourseCategories></CourseCategories>
+      </div>
+      <div className="col-span-9 text-2xl border-2 me-3 p-5">
+        <h1>Mahim</h1>
+      </div>
     </div>
   );
 };

9fcb4b246cabdde96c19964a2785575057dc4137
Sazzad Hosen <sajjadhosenmahim@gmail.com>
2023-04-07 07:45:11 +0600

    instructor image fixed

--- src/Pages/Courses/CourseDetails/CourseDetails.js
+++ src/Pages/Courses/CourseDetails/CourseDetails.js
@@ -3,16 +3,20 @@ import { useLoaderData } from "react-router-dom";
 
 const CourseDetails = () => {
   const singleCourseData = useLoaderData();
-  const { title, image_url, details, author, total_view } = singleCourseData;
+  const { title, details, author, total_view } = singleCourseData;
   console.log(singleCourseData);
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={image_url} className="max-w-sm rounded-lg shadow-2xl"></img>
-          <div>
+        <div className="ms-5 hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={author.img}
+            className=" max-w-xl rounded-lg shadow-2xl"
+          ></img>
+          <div className="me-10">
             <h1 className="text-5xl font-bold">{title}</h1>
-            <p className="py-6 text-lg">{details}</p>
+            <p className="text-2xl pt-10 text-start">Author: {author.name}</p>
+            <p className="py-6 text-lg text-start">{details}</p>
 
             <button className="btn btn-primary">Checkout</button>
           </div>
